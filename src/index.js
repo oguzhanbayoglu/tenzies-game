ad4a3d0924d03f6cc30988a4e8791f31a9ac98c9
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2022-09-18 23:26:34 +0300

    confetti update

--- src/index.js
+++ src/index.js
@@ -76,18 +76,20 @@ function App() {
   ));
 
   return (
-    <div className="container">
-      <header>
-        <h1>Tenzies</h1>
-        <p>
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
-        </p>
-      </header>
-      <div className="dice">{diceElements}</div>
-      <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+    <>
       {tenzies && <Confetti />}
-    </div>
+      <div className="container">
+        <header>
+          <h1>Tenzies</h1>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </header>
+        <div className="dice">{diceElements}</div>
+        <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+      </div>
+    </>
   );
 }
 
