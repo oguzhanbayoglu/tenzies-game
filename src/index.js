88d9129b72427677674a97da062a87e7d71fef45
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2022-10-12 20:51:14 +0300

    classname

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ function App() {
       {tenzies && <Confetti />}
       <div className="container">
         <header>
-          <h1>Tenzies</h1>
+          <h1 className="header">Tenzies</h1>
           <p>
             Roll until all dice are the same. Click each die to freeze it at its
             current value between rolls.
