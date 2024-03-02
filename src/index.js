fe486eca083f9cbc50105f397e87b827ff7bf0f9
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2024-03-03 02:44:31 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./styles.css";
 import ReactDOM from "react-dom";
 import Confetti from "react-confetti";
+import logo from "./logo.png";
 
 function Die(props) {
   return (
     <div
       onClick={props.holdDice}
-      className="die"
+      className="die h-16 w-16 md:h-[6rem] md:w-[6rem]"
       style={{ backgroundColor: props.isHeld ? "#59E391" : "" }}
     >
       <h2>{props.value}</h2>
@@ -78,16 +79,24 @@ function App() {
   return (
     <>
       {tenzies && <Confetti />}
-      <div className="container">
-        <header>
-          <h1 className="header">Tenzies</h1>
-          <p>
-            Roll and roll until all dice are the same. Click each die to freeze
-            it at its current value between rolls.
-          </p>
-        </header>
-        <div className="dice">{diceElements}</div>
-        <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+      <div className="flex flex-col justify-between h-full">
+        <div className="h-40 w-full flex items-center justify-center">
+          <img src={logo} alt="" className="h-16 w-16" />
+        </div>
+        <div className="container w-[27rem] md:w-[40rem]">
+          <header>
+            <h1 className="header">Tenzies</h1>
+            <p>
+              Roll and roll until all dice are the same. Click each die to
+              freeze it at its current value between rolls.
+            </p>
+          </header>
+          <div className="dice">{diceElements}</div>
+          <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+        </div>
+        <footer className="flex items-center justify-center w-full text-white">
+          <p>Oguzhan Bayoglu - 2024 ©</p>
+        </footer>
       </div>
     </>
   );
